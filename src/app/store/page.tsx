fc62dbbff46e56edc622e1e3c1adefa05cd260f2
ayishaatwork <fidhaayisha2004@gmail.com>
2025-12-27 15:48:10 +0530

    Update page.tsx

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -43,6 +43,24 @@ export default async function StorePage() {
         ))}
       </div>
     </section>
+    <footer className="site-footer page-footer">
+        <div className="footer-grid">
+          <Link href="/">Home</Link>
+          <Link href="/shipping-and-returns">Shipping and returns</Link>
+
+          <Link href="/store">Store</Link>
+          <Link href="/payment-information">Payment Information</Link>
+
+          <Link href="/collective">Collective</Link>
+          <Link href="/terms-and-conditions">Terms and Conditions</Link>
+
+          <Link href="/contact">Contact us</Link>
+          <Link href="/privacy-policy">Privacy Policy</Link>
+        </div>
+
+        <div className="footer-copyright">© 2025 Mashi, Inc.</div>
+      </footer>
   );
 }
+
 
